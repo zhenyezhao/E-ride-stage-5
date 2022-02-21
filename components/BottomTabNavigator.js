bf2e9487237aac09062fa63c09bf16f2047c05e0
zhenyezhao <86699069+zhenyezhao@users.noreply.github.com>
2022-02-20 19:47:01 -0800

    Add files via upload

--- components/BottomTabNavigator.js
+++ components/BottomTabNavigator.js
@@ -18,13 +18,13 @@ export default class BottomTabNavigator extends Component {
             tabBarIcon: ({ focused, color, size }) => {
               let iconName;
 
-              if (route.name === "Ride") {
-                iconName = "bicycle";
-              } else if (route.name === "Ride History") {
-                iconName = "time";
-              }
 
-              // You can return any component that you like here!
+              if (route.name === "Ride") {
+                 iconName = "bicycle";
+               } else if (route.name === "Ride History") {
+                 iconName = "time";
+               }
+
               return (
                 <Ionicons
                   name={iconName}
--- config.js
+++ config.js
@@ -1,14 +1,13 @@
 import firebase from "firebase";
 require("@firebase/firestore");
 
-var firebaseConfig = {
-  apiKey: "AIzaSyCZrG0BF6LrnRpWZjsPQxNGyMCLByo8HrM",
-  authDomain: "e-ride-549e4.firebaseapp.com",
-  projectId: "e-ride-549e4",
-  storageBucket: "e-ride-549e4.appspot.com",
-  messagingSenderId: "588298110700",
-  appId: "1:588298110700:web:34e3df331c6b1fc95047c9",
-  measurementId: "G-G3K7BWL5LH"
+const firebaseConfig = {
+  apiKey: "AIzaSyAz9i8y5CwjCFghNj2OjHV-y8nClPfqRoA",
+  authDomain: "e-ride-stage-4-75e56.firebaseapp.com",
+  projectId: "e-ride-stage-4-75e56",
+  storageBucket: "e-ride-stage-4-75e56.appspot.com",
+  messagingSenderId: "802877853209",
+  appId: "1:802877853209:web:8b8c80a9306692d5cc6488"
 };
 
 firebase.initializeApp(firebaseConfig);
